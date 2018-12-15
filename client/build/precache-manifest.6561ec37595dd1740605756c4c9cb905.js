693f4084730cc76ac9162d00ccc792cef1d0355d
Sudipto <sudiptog81@gmail.com>
2018-12-15 11:48:02 +0530

    initial commit

---
+++ client/build/precache-manifest.6561ec37595dd1740605756c4c9cb905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80381eb77c50ff959609",
+    "url": "/static/css/main.0a8b522c.chunk.css"
+  },
+  {
+    "revision": "80381eb77c50ff959609",
+    "url": "/static/js/main.80381eb7.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c493eb5cf1fc34192ee4ef49b62a02b",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/components/Customers/Customers.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./Customers.css";
+
+class Customers extends Component {
+  state = {
+    customers: []
+  };
+  componentDidMount() {
+    fetch("/api/customers")
+      .then(res => res.json())
+      .then(customers => this.setState({ customers }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>React + Express = ReactExpress</h1>
+        <h2>
+          <a href="/api/customers">Fake Data from ExpressAPI</a>
+        </h2>
+        <ul>
+          {this.state.customers.map(customer => (
+            <li key={customer.id}>
+              <div>
+                <img src={customer.avatar} alt={customer.firstName} />
+              </div>
+              <div>
+                {customer.firstName} {customer.lastName}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Customers;
